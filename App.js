f5607bcd7ecc6d3008b104beaeba4ded965f56a2
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-04-09 19:02:10 +0300

    feat: lecture 8

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const initialText = 'Open up App.js to start working on your app!';
+  const newText = 'Text has changed!';
+  const [outputText, setOutputText] = useState(initialText);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <Text>{ outputText }</Text>
+      <Button 
+        onPress = { () => setOutputText(newText) }
+        title = "Change Text"/>
+      <StatusBar style = "auto" />
     </View>
   );
 }
@@ -19,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
